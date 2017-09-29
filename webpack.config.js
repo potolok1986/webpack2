babc2b24ca8ec423919c422243f25187983c9c5a
Александр <potolok1986@gmail.com>
2017-09-29 14:23:13 +0300

    comment in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,19 @@
-const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path"); // Базовая переменная для Windows и Linux
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // плагин создания html
+// Базовые пути
 var PATHS = {
 	source: path.join(__dirname, 'src'),
 	build: path.join(__dirname,'dst')
 };
 
 module.exports ={
-	entry: PATHS.source + "/index.js",
+	entry: PATHS.source + "/index.js", // точка входа в приложение
 	output: {
-		path: PATHS.build,
-		filename: "[name].js"
+		path: PATHS.build, // путь компиляции
+		filename: "[name].js" // имя фйала
 	},
 	plugins: [
+		// объявление плагина и его настройки
 		new HtmlWebpackPlugin({
 			title: "Webpack 2 app"
 		})
